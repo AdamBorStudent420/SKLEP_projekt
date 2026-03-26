f60010cf3dd8b42e09ccae777f7690eb38695f9e
tomeknfs1234-dev <tomeknfs1234@wp.pl>
2026-03-26 22:21:02 +0100

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from sklep.api import api
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
